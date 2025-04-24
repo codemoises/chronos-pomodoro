1b032dc502b2cb1ab3b6475c38cfac1de41c0fb0
codemoises <88902469+codemoises@users.noreply.github.com>
2025-04-24 16:13:09 -0300

    variáveis css

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import './styles/global.css';
+import './styles/theme.css';
+
 export function App() {
-  return <h1>Olá mundo</h1>;
+  return (
+    <>
+      <h1>Título</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, neque
+        laboriosam similique odit fugit porro ex ratione illo mollitia quos quis
+        tempore enim hic, sapiente dolores maxime, cumque maiores corporis.
+      </p>
+    </>
+  );
 }
