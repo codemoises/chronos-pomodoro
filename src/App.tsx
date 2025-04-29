a9c5bfe3b005c0603d6256b23bd898959a75dc9f
codemoises <88902469+codemoises@users.noreply.github.com>
2025-04-29 16:49:24 -0300

    form do App e style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import Container from './components/Container';
+import CountDown from './components/CountDown';
+import { Cycles } from './components/Cycles';
+import DefaultInput from './components/DefaultInput';
 import Logo from './components/Logo';
 import Menu from './components/Menu';
 
@@ -13,6 +16,33 @@ export function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              id='meuInput'
+              type='text'
+              labelText='task'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
