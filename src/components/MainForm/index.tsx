124b2fb3d54c503709a22d34dcd8f65434c1084c
codemoises <88902469+codemoises@users.noreply.github.com>
2025-05-09 15:38:10 -0300

    Componente Tips

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,6 +8,7 @@ import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinute';
+import { Tips } from '../Tips';
 
 export function MainForm() {
   const { state, setState } = useTaskContext();
@@ -84,7 +85,7 @@ export function MainForm() {
       </div>
 
       <div className='formRow'>
-        <p>Próximo intervalo é de 25min</p>
+        <Tips />
       </div>
 
       {state.currentCycle > 0 && (
